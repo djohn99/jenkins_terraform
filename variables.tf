5cb1dd401c53cf0765aa14793eddc0a0e300fdd1
David John <david.ojaigho@gmail.com>
2021-07-18 12:12:23 +0400

    add s3 backend
    Signed-off-by: David John <david.ojaigho@gmail.com>

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region"{
 }
 
 variable "owner" {
-  c
+  default = "default"
   description = "owner"
 }
 variable "shared_credentials_file" {
@@ -27,25 +27,13 @@ variable "jenkins_password" {
   default = "admin"
 }
 
-
-
-
-
-
-
-
-
 // Default variables
-
 variable "vpc_cidr_block" {
   description = "VPC CIDR block"
   default     = "10.0.0.0/16"
 }
 
-
-
 // Jenkins Master
-
 variable "dav-jenkins_instance_type" {
   description = "Jenkins Master instance type"
   default     = "t3.small"
